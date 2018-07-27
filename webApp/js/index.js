1e1e4394d2664763d0836091924e040596c1229e
Celery <934740483@qq.com>
2018-07-27 23:40:52 +0800

    Add or update. -- git_shell.sh

--- webApp/js/index.js
+++ webApp/js/index.js
@@ -5,6 +5,9 @@
 */
 
 $(function () {
+	//处理移动端点击延迟
+	FastClick.attach(document.body);
+
 	// 回到顶部
 	$(window).scroll(function(){
 		if($(window).scrollTop()>=100){
@@ -15,5 +18,28 @@ $(function () {
 	});
 	$(document).on('click','.layui-fixbar li',function(){
 		$('html,body').animate({scrollTop:0},500);
+	});
+
+	// 导航显示
+	$(document).on('click','.header .layui-icon.layui-icon-spread-left',function(){
+		$('.container-header .nav').animate({width:'100%'});
+		$(this).removeClass('layui-icon-spread-left').addClass('layui-icon-shrink-right');
+	});
+	$(document).on('click','.header .layui-icon.layui-icon-shrink-right',function(){
+		$('.container-header .nav').animate({width:'0'});
+		$(this).removeClass('layui-icon-shrink-right').addClass('layui-icon-spread-left');
+	});
+	$(document).on('click','.container-header .nav',function(){
+		$(this).animate({width:'0'});
+		$('.header .layui-icon').removeClass('layui-icon-shrink-right').addClass('layui-icon-spread-left');
+	});
+
+	//顶部轮播图
+	var mySwiper = new Swiper ('.swiper-container', {
+		autoplay: 3000,
+	    loop: true,
+	    // 分页器
+	    pagination: '.swiper-pagination',
+
 	});
 })
