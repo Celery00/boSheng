2db95692f44a71e20a499fd4a92e4106271212a0
Celery <934740483@qq.com>
2018-07-28 10:51:23 +0800

    Add or update. -- git_shell.sh

--- webApp/js/index.js
+++ webApp/js/index.js
@@ -34,7 +34,8 @@ $(function () {
 		// $('.container-wrap').removeClass('overflow');
 		$('.container-main').animate({marginLeft:'0'});
 	});
-	$(document).on('touchend','.container-header .nav',function(){
+	$(document).bind('touchstart','.container-header .nav',function(){
+		console.log(0);
 		$(this).animate({width:'0'});
 		$('.header .layui-icon').removeClass('layui-icon-shrink-right').addClass('layui-icon-spread-left');
 		// $('.container-wrap').removeClass('overflow');
